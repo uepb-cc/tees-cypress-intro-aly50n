4c74ec3e349c5fdbe32304687552d2f82c95214a
aly50n <alyson.morato13@gmail.com>
2023-10-04 08:34:14 -0300

    fix: Correction of the scenario in gerkhin

--- cypress/e2e/site-android.feature
+++ cypress/e2e/site-android.feature
@@ -4,6 +4,4 @@ Feature: Search
 
 Scenario: Search on the website for the word Android
   Given I go to the URL_ANDROID
-  When I visit the website
-  And I navigate to the page
   Then the text "Android" should be visible on the website.
--- cypress/e2e/site-android/search.cy.js
+++ cypress/e2e/site-android/search.cy.js
@@ -3,14 +3,6 @@ Given("I go to the URL_ANDROID", () => {
     cy.visit('/Projeto-Site-Android');
 });
 
-When(/^I visit the website$/, () => {
-	return true;
-});
-
-When(/^I navigate to the page$/, () => {
-	return true;
-});
-
 Then(/^the text "([^"]*)" should be visible on the website.$/, (result) => {
     cy.get('body').should('contain', result);
 	return true;
